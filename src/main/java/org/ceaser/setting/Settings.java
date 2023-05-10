d6240a1f8407f3b4f30a7a9f1c52910b13076ecd
neuronsoftml <neuronsoftml@gmail.com>
2023-05-10 15:27:08 +0300

    Добавили - COMMAND_ENCRYPT Добавили - COMMAND_DECRYPT Добавили - COMMAND_BRUTE_FORCE

--- src/main/java/org/ceaser/setting/Settings.java
+++ src/main/java/org/ceaser/setting/Settings.java
@@ -1,8 +1,11 @@
-package org.ceaser;
+package org.ceaser.setting;
 
 public enum Settings {
     MODE_OF_OPERATION_CONSOLE("CONSOLE"),
-    MODE_OF_OPERATION_GUI("GUI");
+    MODE_OF_OPERATION_GUI("GUI"),
+    COMMAND_ENCRYPT("ENCRYPT"),
+    COMMAND_DECRYPT("DECRYPT"),
+    COMMAND_BRUTE_FORCE("BRUTE_FORCE");
 
     private String value;
 
