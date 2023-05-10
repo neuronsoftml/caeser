862d7e92fee0bed4ec9846dc719cfafbbb8bc631
neuronsoftml <neuronsoftml@gmail.com>
2023-05-10 15:32:41 +0300

    Добавили новий клас TextService.java для аналізу тексту.
    isTextLanguageUkraine - перевіряє чи є данний текст  уркаїнським. Допоміжний метот checkTextLanguageUkraine.
    
    isTextLanguageEnglish - перевіряє чи є данний текст  англійським. Допоміжний метот checkTextLanguageEnglish.

---
+++ src/main/java/org/ceaser/model/TextService.java
@@ -0,0 +1,52 @@
+package org.ceaser.model;
+
+import org.ceaser.setting.DateAlphabet;
+
+public class TextService {
+
+    public boolean isTextLanguageUkraine(String text){
+        if(checkTextLanguageUkraine(text) > checkTextLanguageEnglish(text)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isTextLanguageEnglish(String text){
+        if(checkTextLanguageUkraine(text) < checkTextLanguageEnglish(text)){
+            return true;
+        }
+        return false;
+    }
+
+    private int checkTextLanguageUkraine(String text){
+        char[] dataArrayAlphabet = DateAlphabet.UKRAINIAN.getDate().toCharArray();
+        char[] dataArrayMessage = text.toCharArray();
+
+        int result = 0;
+        for(int i = 0; i < dataArrayMessage.length; i++){
+            for(int j = 0; j < dataArrayAlphabet.length; j++){
+                if(String.valueOf(dataArrayMessage[i]).equals(String.valueOf(dataArrayAlphabet[j]))){
+                    result++;
+                    break;
+                }
+            }
+        }
+        return  result;
+    }
+
+    private int checkTextLanguageEnglish(String text){
+        char[] dataArrayAlphabet = DateAlphabet.ENGLISH.getDate().toCharArray();
+        char[] dataArrayMessage = text.toCharArray();
+
+        int result = 0;
+        for(int i = 0; i < dataArrayMessage.length; i++){
+            for(int j = 0; j < dataArrayAlphabet.length; j++){
+                if(String.valueOf(dataArrayMessage[i]).equals(String.valueOf(dataArrayAlphabet[j]))){
+                    result++;
+                    break;
+                }
+            }
+        }
+        return  result;
+    }
+}
