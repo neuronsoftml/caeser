8f8183a86e82bbde3362a9c18d124505a9b12942
neuronsoftml <neuronsoftml@gmail.com>
2023-05-10 15:38:25 +0300

    Добавили нову опцию checkCommand(параметр дії). checkCommand - аналізовує яку функцію для Цезаря був вибраний користувачем. Та запускає вибрану функцію. - runDecrypt(); - runEncryption(); - runBrutForce();

--- src/main/java/org/ceaser/controller/ControllerConsole.java
+++ src/main/java/org/ceaser/controller/ControllerConsole.java
@@ -1,8 +1,45 @@
 package org.ceaser.controller;
 
+import org.ceaser.model.CaesarCipher;
+import org.ceaser.model.FileService;
+import org.ceaser.setting.Settings;
+
 public class ControllerConsole {
+    private String command;
+    private String filepath;
+    private int key;
+
+    public ControllerConsole(String command, String filepath, int key) {
+        this.command = command;
+        this.filepath = filepath;
+        this.key = key;
+    }
 
     public void run() {
-        System.out.println("Запуск програми в консолі");
+        checkCommand(command);
     }
+
+    private void checkCommand(String command){
+        if(command.equals(Settings.COMMAND_ENCRYPT.getValue())){
+            runEncryption();
+        } else if (command.equals(Settings.COMMAND_DECRYPT.getValue())) {
+            runDecrypt();
+        }
+    }
+
+    private void runDecrypt() {
+        CaesarCipher caesarCipher = new CaesarCipher(key,filepath);
+        caesarCipher.decryption();
+    }
+
+    private void runEncryption() {
+        CaesarCipher caesarCipher = new CaesarCipher(key,filepath);
+        caesarCipher.encryption();
+    }
+
+    private void runBrutForce(){
+        CaesarCipher caesarCipher = new CaesarCipher(filepath);
+        caesarCipher.brutForce();
+    }
+
 }
