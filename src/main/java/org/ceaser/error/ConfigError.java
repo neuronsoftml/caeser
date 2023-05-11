1c8ce2914968e034a3f5391d714ce4fc2d8542cb
neuronsoftml <neuronsoftml@gmail.com>
2023-05-11 14:35:39 +0300

    Перейменовано Config на ConfigError.

--- src/main/java/org/ceaser/error/ConfigError.java
+++ src/main/java/org/ceaser/error/ConfigError.java
@@ -1,12 +1,12 @@
 package org.ceaser.error;
 
-public enum Config {
+public enum ConfigError {
     ERROR_COMMAND("Помилка запуску \"command\" not found -c виберіть три доступні варіанти:[ENCRYPT, DECRYPT, BRUTE_FORCE]"),
     ERROR_FILE_PATH("Помилка запуску \"filepath\" not found  -f абсолютний(повний) шлях до файлу, який кодується."),
     ERROR_MODE_OF_OPERATION("Помилка запуску \"modeOfOperations\" not found  -m режим запуску програми [CONSOLE, GUI]"),
     ERROR_REGULAR_FILE("Помилка \"URL File\" not found такого файла не їснує.");
 
-    Config(String text){
+    ConfigError(String text){
         this.text = text;
     }
     private String text;
