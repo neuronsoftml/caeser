e3f9d61f56403b5eb92749ae65342d44c73c4af7
neuronsoftml <neuronsoftml@gmail.com>
2023-05-14 21:22:14 +0300

    Було добавлено збільшення атаки у випадку неуспішного аналізу.

--- src/main/java/org/ceaser/model/CaesarCipher.java
+++ src/main/java/org/ceaser/model/CaesarCipher.java
@@ -1,6 +1,9 @@
 package org.ceaser.model;
 
-import org.ceaser.setting.DateAlphabet;
+import org.ceaser.setting.LettersAlphabet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CaesarCipher {
     private String dateAlphabet;
@@ -17,26 +20,30 @@ public class CaesarCipher {
         this.text = fileService.readFile(filePath);
         setTheAlphabet(text);
         checkSelectKey(key);
-        this.dataArrayAlphabetToUp = dateAlphabet.toUpperCase().toCharArray();
-        this.dataArrayAlphabetToLower = dateAlphabet.toLowerCase().toCharArray();
+        setDataArrayAlphabet();
 
         this.dataText = text.toCharArray();
     }
     public CaesarCipher(String filePath) {
+        this.filePath = filePath;
         this.text = fileService.readFile(filePath);
         setTheAlphabet(text);
-        this.dataArrayAlphabetToUp = this.dateAlphabet.toUpperCase().toCharArray();
-        this.dataArrayAlphabetToLower = this.dateAlphabet.toLowerCase().toCharArray();
+        setDataArrayAlphabet();
 
-        this.dataText = this.text.toCharArray();
+        this.dataText = text.toCharArray();
+    }
+
+    private void setDataArrayAlphabet(){
+        this.dataArrayAlphabetToUp = dateAlphabet.toUpperCase().toCharArray();
+        this.dataArrayAlphabetToLower = dateAlphabet.toLowerCase().toCharArray();
     }
 
     private void setTheAlphabet(String text){
         TextService textService = new TextService();
         if (textService.isTextLanguageEnglish(text)) {
-            this.dateAlphabet = DateAlphabet.ENGLISH.getDate();
+            this.dateAlphabet = LettersAlphabet.ENGLISH.getDate();
         } else if (textService.isTextLanguageUkraine(text)) {
-            this.dateAlphabet = DateAlphabet.UKRAINIAN.getDate();
+            this.dateAlphabet = LettersAlphabet.UKRAINIAN.getDate();
         }
     }
 
@@ -125,7 +132,57 @@ public class CaesarCipher {
         return 0;
     }
 
+    private int getIndexOffsetByKeyBrutForce(int start, int key){
+        int indexAlphabet = start;
+        for(int x = 0; x < key; x++){
+            if(indexAlphabet == 0){
+                indexAlphabet = dataArrayAlphabetToUp.length-1;
+            }else {
+                indexAlphabet--;
+            }
+            if(x == key-1){
+                return indexAlphabet;
+            }
+        }
+        return 0;
+    }
+
     public void brutForce() {
+        int startKey = 110;
+        int endKey = 125;
+       while (true){
+           List<String> decipheredVariantsOfText = new ArrayList<>();
+           System.out.println(startKey+ " "+endKey);
+           for (int key = startKey; key < endKey; key++) {
+               StringBuilder result = new StringBuilder();
+               for (int i = 0; i < dataText.length; i++) {
+                   for (int y = 0; y < dataArrayAlphabetToUp.length; y++) {
+                       int indexNewChar;
+                       if (dataArrayAlphabetToLower[y] == dataText[i]) {
+                           indexNewChar = getIndexOffsetByKeyBrutForce(y,key);
+                           result.append(dataArrayAlphabetToLower[indexNewChar]);
+                           break;
+                       } else if (dataArrayAlphabetToUp[y] == dataText[i]) {
+                           indexNewChar = getIndexOffsetByKeyBrutForce(y,key);
+                           result.append(dataArrayAlphabetToUp[indexNewChar]);
+                           break;
+                       }
+                   }
+               }
+               decipheredVariantsOfText.add(result.toString());
+           }
+           AnalysisManager analysisManager = new AnalysisManager(dateAlphabet);
+           if(analysisManager.start(decipheredVariantsOfText) == null){
+               startKey =  endKey;
+               endKey = startKey +30;
+
+           }
+           else {
+               System.out.println(analysisManager.start(decipheredVariantsOfText));
+               break;
+           }
+       }
+
 
     }
 }
