aef829956883ba0761b8e1dec658877633e74b74
neuronsoftml <neuronsoftml@gmail.com>
2023-05-18 17:42:41 +0300

    Було добавлено два нових метота getDataAlphabetToUp та getDataAlphabetToLower об'єднує та виводить вибраний алфавіт з спец синволами.

--- src/main/java/org/ceaser/setting/LettersAlphabet.java
+++ src/main/java/org/ceaser/setting/LettersAlphabet.java
@@ -3,11 +3,21 @@ package org.ceaser.setting;
 public enum LettersAlphabet {
     ENGLISH("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
     UKRAINIAN("АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩьЮЯ"),
-    SPECIAL_CHARACTERS_ALL(".,\":-!? «»\\");
+    SPECIAL_CHARACTERS_ALL(".,\":-!? «»\\"),
+    DATA_ALPHABET("");
+
 
     private String date;
     LettersAlphabet(String s) {
         this.date = s;
+    }
+
+    public String getDataAlphabetToUp(String dateAlphabet){
+        return dateAlphabet + SPECIAL_CHARACTERS_ALL.getDate();
+    }
+
+    public String getDataAlphabetToLower(String dateAlphabet){
+        return dateAlphabet.toLowerCase() + SPECIAL_CHARACTERS_ALL.getDate().toLowerCase();
     }
 
     public String getDate(){
