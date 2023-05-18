9ea2d58a3a3d6e55a1a5d70e8cfeaa294b9079c2
neuronsoftml <neuronsoftml@gmail.com>
2023-05-18 15:37:08 +0300

    Було добавлено нові медоти  checkParameters, validateKey validateCommand validateModeOfOperation що допомогають у валідації.

--- src/main/java/org/ceaser/model/CLI.java
+++ src/main/java/org/ceaser/model/CLI.java
@@ -2,6 +2,7 @@ package org.ceaser.model;
 
 import org.apache.commons.cli.*;
 import org.ceaser.error.ConfigError;
+import org.ceaser.setting.ConfigCLI;
 
 public class CLI {
     private final String[] settings;
@@ -13,10 +14,9 @@ public class CLI {
     public void start() throws ParseException {
         definitionOfParameters();
         analysisOfParameters();
-        System.out.println(command);
-        System.out.println(filepath);
-        System.out.println(key);
-        checkParameters();
+        System.out.println("Команда :"+command);
+        System.out.println("Шлях до файла :"+filepath);
+        System.out.println("Ключь :"+key);
     }
 
     private Options options;
@@ -38,61 +38,111 @@ public class CLI {
         this.options = options;
     }
 
-    private String command;
-    private String filepath;
-    private int key;
-    private String modeOfOperation;
-
     private void analysisOfParameters() throws ParseException {
         CommandLineParser commandLineParser = new DefaultParser();
         CommandLine cmd = commandLineParser.parse(options, settings);
 
         cmd.hasOption("c");
         this.command = cmd.getOptionValue("c");
-
         cmd.hasOption("f");
         this.filepath = cmd.getOptionValue("f");
-
         cmd.hasOption("m");
         this.modeOfOperation = cmd.getOptionValue("m");
-
         cmd.hasOption("k");
-        String key = cmd.getOptionValue("k");
-        if(key == null){
-            this.key = 0;
-        }else {
-            this.key = Integer.parseInt(cmd.getOptionValue("k"));
-        }
+        this.key = cmd.getOptionValue("k") == null ? 0 : Integer.parseInt(cmd.getOptionValue("k"));
 
+        checkParameters(
+                command,
+                filepath,
+                modeOfOperation,
+                key
+        );
     }
 
-    private void checkParameters(){
-        if(command == null){
-            System.out.println(ConfigError.ERROR_COMMAND.getText());
-            System.exit(0);
-        }
-        else if(filepath == null){
-            System.out.println(ConfigError.ERROR_FILE_PATH.getText());
-            System.exit(0);
-        }
-        else if (modeOfOperation ==  null) {
-            System.out.println(ConfigError.ERROR_MODE_OF_OPERATION.getText());
-            System.exit(0);
-        }
-    }
-
+    private String command;
     public String getCommand() {
         return command;
     }
 
+    private String filepath;
     public String getFilepath() {
         return filepath;
     }
 
+    private int key;
     public int getKey() {
         return key;
     }
 
+    private String modeOfOperation;
     public String getModeOfOperation(){return modeOfOperation;}
 
+    private void checkParameters(String command, String filepath, String modeOfOperation, int key){
+        if(!validateCommand(command)){
+            System.exit(0);
+        }
+        else if(!validateModeOfOperation(modeOfOperation)){
+            System.exit(0);
+        }
+        else if (!validateFilePath(filepath)) {
+            System.exit(0);
+        }
+        else if(!validateKey(key)){
+            System.exit(0);
+        }
+    }
+
+    private boolean validateKey(int key){
+        if(key == 0 && modeOfOperation.equals(ConfigCLI.COMMAND_BRUTE_FORCE.getValue())){
+            return true;
+        }
+        else if(key <= 0){
+            System.out.println(ConfigError.ERROR_KEY.getText());
+            return false;
+        }
+        this.key = key;
+        return true;
+    }
+
+    private boolean validateCommand(String command){
+        if(command == null){
+            System.out.println(ConfigError.ERROR_COMMAND.getText());
+            return false;
+
+        } else if (
+                !command.equals(ConfigCLI.COMMAND_ENCRYPT.getValue()) &&
+                !command.equals(ConfigCLI.COMMAND_DECRYPT.getValue()) &&
+                !command.equals(ConfigCLI.COMMAND_BRUTE_FORCE.getValue()) ) {
+
+            System.out.println(ConfigError.ERROR_COMMAND.getText());
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateModeOfOperation(String modeOfOperation){
+        if(modeOfOperation == null){
+            System.out.println(ConfigError.ERROR_MODE_OF_OPERATION.getText());
+            return false;
+
+        } else if (
+                !modeOfOperation.equals(ConfigCLI.MODE_OF_OPERATION_CONSOLE.getValue()) &&
+                        !modeOfOperation.equals(ConfigCLI.MODE_OF_OPERATION_GUI.getValue())) {
+
+            System.out.println(ConfigError.ERROR_COMMAND.getText());
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateFilePath(String filepath) {
+        if(filepath == null){
+            System.out.println(ConfigError.ERROR_FILE_PATH.getText());
+            return false;
+
+        }
+        return true;
+    }
 }
