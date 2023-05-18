84000d30baf156d800f1fce0eb6ac8e7eb7b33e1
neuronsoftml <neuronsoftml@gmail.com>
2023-05-18 17:49:58 +0300

    Було добавлено запісь у файл після отримання результатів проведеня операції  runDecrypt runEncryption runBrutForce.
    Було видалено зайвий метод runErrorNotCommand()

--- src/main/java/org/ceaser/controller/ControllerConsole.java
+++ src/main/java/org/ceaser/controller/ControllerConsole.java
@@ -2,16 +2,17 @@ package org.ceaser.controller;
 
 import org.ceaser.error.ConfigError;
 import org.ceaser.model.CaesarCipher;
+import org.ceaser.model.FileService;
 import org.ceaser.setting.ConfigCLI;
 
 public class ControllerConsole {
     private String command;
-    private String filepath;
+    private String filePath;
     private int key;
 
-    public ControllerConsole(String command, String filepath, int key) {
+    public ControllerConsole(String command, String filePath, int key) {
         this.command = command;
-        this.filepath = filepath;
+        this.filePath = filePath;
         this.key = key;
     }
 
@@ -26,29 +27,22 @@ public class ControllerConsole {
             runDecrypt();
         } else if (command.equals(ConfigCLI.COMMAND_BRUTE_FORCE.getValue())) {
             runBrutForce();
-        }else{
-            runErrorNotCommand();
         }
     }
 
     private void runDecrypt() {
-        CaesarCipher caesarCipher = new CaesarCipher(key,filepath);
-        caesarCipher.decryption();
+        CaesarCipher caesarCipher = new CaesarCipher(key, filePath);
+        new FileService().writerFile(caesarCipher.decryption(), filePath, "\\[DECRYPTED].txt");
     }
 
     private void runEncryption() {
-        CaesarCipher caesarCipher = new CaesarCipher(key,filepath);
-        caesarCipher.encryption();
+        CaesarCipher caesarCipher = new CaesarCipher(key, filePath);
+        new FileService().writerFile(caesarCipher.encryption(), filePath, "\\[ENCRYPTED].txt");
     }
 
     private void runBrutForce(){
-        CaesarCipher caesarCipher = new CaesarCipher(filepath);
-        caesarCipher.brutForce();
-    }
-
-    private void runErrorNotCommand(){
-        System.out.println(ConfigError.ERROR_COMMAND.getText());
-        System.exit(0);
+        CaesarCipher caesarCipher = new CaesarCipher(filePath);
+        new FileService().writerFile(caesarCipher.brutForce(), filePath, "\\[BRUTE_FORCE].txt");
     }
 
 }
